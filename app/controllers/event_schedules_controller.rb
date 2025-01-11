ff83155b86e69a1e85858507bba4e21a662326ca
ryusei42 <ryuseitakata27@gmail.com>
2025-01-11 15:37:46 +0900

    Finish schedule_app

---
+++ app/controllers/event_schedules_controller.rb
@@ -0,0 +1,56 @@
+class EventSchedulesController < ApplicationController
+  before_action :set_event_schedule, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @event_schedules = EventSchedule.all
+    @schedule_count = @event_schedules.count
+    @current_date = Time.current.strftime("%Y/%m/%d")
+  end
+
+  def show
+  end
+
+  def new
+    @event_schedule = EventSchedule.new
+  end
+
+  def create
+    @event_schedule = EventSchedule.new(event_schedule_params)
+    if @event_schedule.save
+      redirect_to event_schedules_path, notice: 'スケジュールを作成しました。'
+    else
+      flash.now[:alert] = 'スケジュールの作成に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @event_schedule.update(event_schedule_params)
+      redirect_to event_schedule_path(@event_schedule), notice: 'スケジュールを更新しました。'
+    else
+      flash.now[:alert] = 'スケジュールの更新に失敗しました。'
+      render :edit
+    end
+  end
+
+  def destroy
+    @event_schedule = EventSchedule.find(params[:id])
+    @event_schedule.destroy
+    redirect_to event_schedules_path, notice: 'スケジュールを削除しました。'
+  rescue ActiveRecord::RecordNotFound
+    redirect_to event_schedules_path, alert: '削除対象のスケジュールが見つかりませんでした。'
+  end
+
+  private
+
+  def set_event_schedule
+    @event_schedule = EventSchedule.find(params[:id])
+  end
+
+  def event_schedule_params
+    params.require(:event_schedule).permit(:title, :start_date, :end_date, :all_day, :memo)
+  end
+end
